ee08fc5a02be3106a5aeb33dea66eab0f3b800ed
kumarbhupi <kumar.bhupi13@gmail.com>
2021-12-13 00:06:08 +0100

    demo.py modified

--- demo/demo.py
+++ demo/demo.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('../data/detect_dataset.csv')
+dataset = pd.read_csv('data/detect_dataset.csv')
 dataset = dataset.drop(['Unnamed: 7', 'Unnamed: 8'], axis=1)
 
 Y = dataset.values[:, 0]
@@ -33,54 +33,54 @@ from joblib import dump
 
 clf_svc_poly = SVC(probability=True, kernel='poly', degree=2)
 clf_svc_poly.fit(X_train, y_train)
-dump(clf_svc_poly, '../models/clf_svc_poly.joblib')
+dump(clf_svc_poly, 'models/clf_svc_poly.joblib')
 
 
 clf_svc_rbf = SVC(probability=True, kernel = 'rbf')
 clf_svc_rbf.fit(X_train, y_train)
-dump(clf_svc_rbf, '../models/clf_svc_rbf.joblib')
+dump(clf_svc_rbf, 'models/clf_svc_rbf.joblib')
 
 
 clf_lr = LogisticRegression()
 clf_lr.fit(X_train, y_train)
-dump(clf_lr, '../models/clf_lr.joblib')
+dump(clf_lr, 'models/clf_lr.joblib')
 
 
 clf_gbc = GradientBoostingClassifier()
 clf_gbc.fit(X_train, y_train)
-dump(clf_gbc, '../models/clf_gbc.joblib')
+dump(clf_gbc, 'models/clf_gbc.joblib')
 
 
 clf_dtc = DecisionTreeClassifier()
 clf_dtc.fit(X_train, y_train)
-dump(clf_dtc, '../models/clf_dtc.joblib')
+dump(clf_dtc, 'models/clf_dtc.joblib')
 
 clf_mlp = MLPClassifier()
 clf_mlp.fit(X_train, y_train)
-dump(clf_mlp, '../models/clf_mlp.joblib')
+dump(clf_mlp, 'models/clf_mlp.joblib')
 
 
 from joblib import load
 
-clf_svc_poly = load('clf_svc_poly.joblib')
+clf_svc_poly = load('models/clf_svc_poly.joblib')
 print('SCV POLY Score:', clf_svc_poly.score(X_test, y_test))
 
 
-clf_svc_rbf = load('clf_svc_rbf.joblib')
+clf_svc_rbf = load('models/clf_svc_rbf.joblib')
 print('SCV RBF Score:', clf_svc_rbf.score(X_test, y_test))
 
 
-clf_lr = load('clf_lr.joblib')
+clf_lr = load('models/clf_lr.joblib')
 print('Logistic Regression Score:', clf_lr.score(X_test, y_test))
 
 
-clf_gbc = load('clf_gbc.joblib')
+clf_gbc = load('models/clf_gbc.joblib')
 print('Gradient Boosting Classifier Score:', clf_gbc.score(X_test, y_test))
 
 
-clf_dtc = load('clf_dtc.joblib')
+clf_dtc = load('models/clf_dtc.joblib')
 print('Decision Tree Classifier Score:', clf_dtc.score(X_test, y_test))
 
 
-clf_mlp = load('clf_mlp.joblib')
+clf_mlp = load('models/clf_mlp.joblib')
 print('Perceptron Score:', clf_mlp.score(X_test, y_test))
